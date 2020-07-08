d0c09def5d4ca712459b37ddb0dd861522b135e7
16091221 <67929833+16091221@users.noreply.github.com>
2020-07-08 16:44:09 -0500

    Revert "Update calculadora.php"
    This reverts commit 37d72bb046413f80e4f76382772839737dafab8d.

--- public/calculadora.php
+++ public/calculadora.php
@@ -27,18 +27,3 @@ function calculadora($request){
     return json_encode($respuesta);
 
 }
-function pitagoras($request){
-
-    $data=json_decode($request->getbody());
- 
-     $a=$data->a;
-     $b=$data->b;
-     $a2=$a*$a;
-     $b2=$b*$b;
-     $c2=$a2+$b2;
-     $c=sqrt($c2);
-     $respuesta->hipotenusa=$c;
- 
-     
-     return json_encode($respuesta);
- }
