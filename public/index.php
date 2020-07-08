59caaae2beec67f2b7eaaa645c8371c57f7d69c7
16091221 <67929833+16091221@users.noreply.github.com>
2020-07-08 18:13:40 -0500

    Pitagoras

--- public/index.php
+++ public/index.php
@@ -45,6 +45,8 @@ include 'funciones/distancia.php';
 include 'funciones/pentagono.php';
 include 'funciones/16091065_area.php';
 include 'funciones/16091246_Trapecio.php';
+include 'funciones/16091221_pitagoras.php';
+
 // Run app
 $app->run();
 
