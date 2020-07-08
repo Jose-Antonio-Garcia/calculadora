e3b86e946e63664524179f9c44b8328832bd5a49
FelipeVicera <67929801+FelipeVicera@users.noreply.github.com>
2020-07-08 16:06:02 -0500

    Add files via upload

--- public/calculadora.php
+++ public/calculadora.php
@@ -27,3 +27,22 @@ function calculadora($request){
     return json_encode($respuesta);
 
 }
+
+function distancia($request){
+    $datos = json_decode($request->getbody());
+    
+    $x1 = $datos->x1;
+    $y1 = $datos->y1;
+    $x2 = $datos->x2;
+    $y2 = $datos->y2;
+
+    $a1 = $x2-$x1;
+    $a2 = $y2-$y1;
+
+    $b1 = $a1*$a1;
+    $b2 = $a2*$a2;
+
+    $dist = sqrt($b1+$b2);
+    return json_encode($dist);
+
+}
