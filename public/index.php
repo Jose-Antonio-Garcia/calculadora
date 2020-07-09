157820fa74335d542f5ffecff02b675b8367ae9f
jose5717 <jose.garcia@uaem.mx>
2020-07-09 16:33:55 -0500

    Correccion de todas las ramas

--- public/index.php
+++ public/index.php
@@ -40,6 +40,13 @@ require_once __DIR__ . '/../includes/DbConnect.php';
 ////////////////////////////////////////////////
 
 
-include 'calculadora.php';
+include 'funciones/calculadora.php';
+include 'funciones/distancia.php';
+include 'funciones/pentagono.php';
+include 'funciones/16091065_area.php';
+include 'funciones/16091246_Trapecio.php';
+include 'funciones/16091221_pitagoras.php';
+include 'funciones/16091216_Binomio.php';
 // Run app
 $app->run();
+
