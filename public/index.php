585f841840a4243a895acd879713db46a8452d7c
CarlosMichaca <67929792+CarlosMichaca@users.noreply.github.com>
2020-07-09 17:43:43 -0500

    PuntoMedio

--- public/index.php
+++ public/index.php
@@ -47,6 +47,7 @@ include 'funciones/16091065_area.php';
 include 'funciones/16091246_Trapecio.php';
 include 'funciones/16091221_pitagoras.php';
 include 'funciones/16091216_Binomio.php';
+include 'funciones/16091299_PuntoMedio.php';
 // Run app
 $app->run();
 
